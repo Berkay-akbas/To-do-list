59f5d83e2e328dea552237ef006fab68e5c85b57
Berkay Akbas <berkayakbas@gmail.com>
2022-07-05 16:29:02 +0300

    Initial setup

---
+++ print.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkwebpack_exercise"] = self["webpackChunkwebpack_exercise"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
